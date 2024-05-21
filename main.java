339bfad7a5bbf5334ea61e909ae9b7c1c34a985a
MonaPB <mona.pourbideh@gmail.com>
2024-05-21 15:43:04 +0330

    commit asghar

--- main.java
+++ main.java
@@ -1,7 +1,20 @@
 public class main {
     public static void main(String[] args) {
 
-        System.out.println("Hello Java");
+        long age=30;
+        long height=1300004546874534L;
+        long a= Long.MAX_VALUE;
+        System.out.println(a);
+
+        double big=12523.265564;
+        System.out.println(big);
+
+        boolean mona=true;
+        boolean mina=false;
+        System.out.println(mona);
+
+        char n = 'n';
+
     }
 }
 
